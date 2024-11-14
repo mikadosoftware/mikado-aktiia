767aeba05b2abf88e36ec5e864d880003e5f81c3
Paul Brian <paul@mikadosoftware.com>
2024-11-14 15:17:16 +0000

    works so far

--- mikado/aktiia/aktiia_utils.py
+++ mikado/aktiia/aktiia_utils.py
@@ -4,9 +4,37 @@ from pprint import pprint as pp
 import pandas as pd
 
 def convert_dual_row(row):
-    lh_date, lh_year, lh_time, lh_sbp, lh_dbp, lh_hr, rh_date, rh_year, rh_time, rh_sbp, rh_dbp, rh_hr,_ = row
-    lh = [lh_date + " " + lh_year + " " + lh_time, lh_sbp, lh_dbp, lh_hr]
-    rh = [rh_date + " " + rh_year + " " + rh_time, rh_sbp, rh_dbp, rh_hr]
+    if len(row) == 13:
+        lh_date, lh_year, lh_time, lh_sbp, lh_dbp, lh_hr, rh_date, rh_year, rh_time, rh_sbp, rh_dbp, rh_hr,_ = row
+        lh = [lh_date + " " + lh_year + " " + lh_time, lh_sbp, lh_dbp, lh_hr]
+        rh = [rh_date + " " + rh_year + " " + rh_time, rh_sbp, rh_dbp, rh_hr]
+    elif len(row) == 14:
+        #['8 November,', '24', '18:34', '135', '89', '65', '9', 'November,', '24', '05:03', '124', '77', '54', '']
+        if len(row[0]) > 2: 
+            lh_date, lh_year, lh_time, lh_sbp, lh_dbp, lh_hr, rh_day, rh_mth, rh_year, rh_time, rh_sbp, rh_dbp, rh_hr,_ = row
+            rh_date = rh_day + " " + rh_mth 
+        else:
+        #['9', 'November,', '24', '19:27', '128', '82 57', '10', 'November,', '24', '21:37', '154', '96', '64', '']
+            lh_day, lh_mth, lh_year, lh_time, lh_sbp, lh_dbp_hr,  rh_day, rh_mth, rh_year, rh_time, rh_sbp, rh_dbp, rh_hr,_ = row
+            rh_date = rh_day + " " + rh_mth 
+            lh_date = lh_day + " " + lh_mth
+            lh_dbp, lh_hr = lh_dbp_hr.split(" ")
+
+        lh = [lh_date + " " + lh_year + " " + lh_time, lh_sbp, lh_dbp, lh_hr]
+        rh = [rh_date + " " + rh_year + " " + rh_time, rh_sbp, rh_dbp, rh_hr]
+
+    elif len(row) == 7:
+        #['1', '1 November,', '24', '15:37', '144', '90 82', '']
+        lh_datetime = row[0]+row[1]+ " " + row[2] + " " + row[3]
+        lh_sbp = row[4]
+        try:
+            lh_dbp, lh_hr = row[5].split(" ")
+        except:
+            import pdb;pdb.set_trace()
+        lh = [lh_datetime, lh_sbp, lh_dbp, lh_hr]
+        rh = None
+    else:
+        import pdb;pdb.set_trace()
     return [lh, rh]
 
 def find_bp_readings(tbl_as_extract):
@@ -14,40 +42,70 @@ def find_bp_readings(tbl_as_extract):
     headerrow = []
     bodyrows = []
     for row in tbl_as_extract:
-        if row[0] == 'DATE':
+        # bad table
+        mightbetext = ''.join(row)
+        if 'BP Readings' in mightbetext:
+            input('readoing')
+        if 'My Notes'.lower() in mightbetext.lower():
+            return None
+        if 'Summary Table'.lower() in mightbetext.lower():
+            return None
+        if 'DATE' in row:  #coul dbe in first or second position 
             headerrow = row
-            print(headerrow)
-            input('gg')
             headerflag = True
             continue
+        
         if headerflag:
+            outflag = False
             if row[0] == '':
                 continue
-            if row[0] == 'Initialization cuff measur':
+            #mightbetext = ''.join(row)
+            badtext = ('inquiries', 'quiries', 'ort', 'offered', 'cuff')
+            for txt in badtext:
+                if txt.lower() in mightbetext.lower():
+                    outflag = True
+            if outflag:
                 continue
-            if row[0] == 'inquiries regarding this repor':
-                continue
-            if row[0] == 'ort was offered to you by Akti':
-                continue
+            # do we have two rows across page?
             lh,rh = convert_dual_row(row)
-            bodyrows.append(lh)
-            bodyrows.append(rh)
+            if lh:
+                bodyrows.append(lh)
+            if rh:
+                bodyrows.append(rh)
     df = pd.DataFrame(bodyrows, columns=['datetime', 'sbp', 'dbp','hr'])
-    print(df)
+    return df
 
 def run():
+
+    file = '/home/pbrian/Downloads/AktiiaReport_pb_Nov2024.pdf'
+    doc = pymupdf.open(file) # open a document
+    tables = pd.DataFrame()
+    for c, page in enumerate(doc):
+        foundtables = page.find_tables(strategy='text')
+        for tbl in foundtables.tables:
+            extract = tbl.extract()
+            df = find_bp_readings(extract)
+            print(f'page: {c} {page.number}')
+            #print(df)
+            if tables.empty:
+                tables = df
+            else:
+                tables = pd.concat([tables, df], axis=0)
+    print(tables)
+
+def run2():
+
     file = '/home/pbrian/Downloads/AktiiaReport_pb_Nov2024.pdf'
     doc = pymupdf.open(file) # open a document
     tables = []
-    for c, page in enumerate(doc):
-        foundtables = page.find_tables(strategy='text')
-        #print(c, len(tbl.tables))
 
-        #import pdb;pdb.set_trace()
-        for tbl in foundtables.tables:
-            extract = tbl.extract()
-            find_bp_readings(extract)
-            input('?')   
+    page = doc[9]
+    foundtables = page.find_tables(strategy='text')
+
+    extract = foundtables.tables[0].extract()
+    import pdb;pdb.set_trace()
+    df = find_bp_readings(extract)
+    print(df)
 
 if __name__ == '__main__':
     run()
