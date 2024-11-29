47d147b18f787ec24c29f8ef4a8be1703bf0c9ea
Paul Brian <paul@mikadosoftware.com>
2024-11-29 16:25:12 +0000

    working output

--- mikado/aktiia/aktiia_utils.py
+++ mikado/aktiia/aktiia_utils.py
@@ -42,46 +42,71 @@ import pymupdf
 from pprint import pprint as pp
 import pandas as pd
 import os
+import datetime
 
 def test_process_rows():
     rows = [
 ['27', 'May,', '24', '16:01', '127', '83', '65', '1 June,', '24', '09:43', '145', '97', '72', ''],
-['1', 'June,', '24', '00:19', '133', '85', '73', '1 June,', '24', '09:51', '131', '83', '62', ''],
-['1', '1', 'June,', '24', '00:36', '151', '98', '72', '1 June,', '24', '09:59', '135', '86', '60']
+['1', 'June,', '24', '00:19', '133', '85', '73', '1 June,', '24', '09:51', '131 83', '62', '', ''],
+['1', '1', 'June,', '24', '00:36', '151', '98', '72', '1 June,', '24', '09:59','135', '86', '60'],
+['1', '1', 'June,', '24', '00:36', '151', '98', '72']
 ]
     for row in rows:
         process_text_row(row)
 
+def convertdates(timestring):
+    """
+    >>> convertdates("1June2400:19")
+    2024-06-01 00:19:00
+    """
+    fmt = "%d%B%y%H:%M"
+    return datetime.datetime.strptime(timestring, fmt)
+
 def process_text_row(row):
-    print(row)
-    s = ' '.join(row)
-    #remove double spaces, clean up characters
-    while '  ' in s:
-        s = s.replace('  ',' ')
-    s = s.replace(',','')
 
-    #split up first set
-    # we want to find the time, which works backawars to everything rles 
-    # 27 May 24 16:01 127 83 65 1 June 24 09:43 145 97 72
+    try:
+        row = eval(row) #not great!
+    except:
+        return (None, None, None, None), (None, None, None, None)
+    row_as_string = ' '.join(row)
+    row_as_string = row_as_string.replace(',','')
+    def extract_timeandreadings(row_as_string):
+
+        #remove double spaces, clean up characters
+        while '  ' in row_as_string:
+            row_as_string = row_as_string.replace('  ',' ')
+
+        #split up first set
+        # we want to find the time, which works backawars to everything rles 
+        # 27 May 24 16:01 127 83 65 1 June 24 09:43 145 97 72
+        
+        # find lh timestamp, then minutes, plus a space 16:01 ]
+        idx = row_as_string.find(":") + 3
+        lh_datetime = row_as_string[:idx].replace(" ", "")
+        # we now should have the datetime, now grab the next three space
+        # deliimited items
+        remains = row_as_string[idx:]
+        lh_bp = remains.strip().split(" ")[:3]
+        delete_bp = ' '.join(lh_bp)
+        remains = remains.replace(delete_bp, '').strip()
+        return lh_datetime, lh_bp, remains
+    lhtimestring, lh_readings, remains = extract_timeandreadings(row_as_string)
+    lh_datetime = convertdates(lhtimestring)
+    lh_sbp, lh_dbp, lh_hr = lh_readings
+
+    if bool(remains):
+        rhtimestring, rh_readings, remains1 = extract_timeandreadings(remains)   
+        rh_datetime = convertdates(rhtimestring)
+        rh_sbp, rh_dbp, rh_hr = rh_readings
+    else:
+        rh_datetime = None
+        rh_sbp, rh_dbp, rh_hr = 0,0,0
     
-    # find lh timestamp, then minutes, plus a space 16:01 ]
-    idx = s.find(":") + 3
-    lh_datetime = s[:idx]
-    # we now should have the datetime
-    print(lh_datetime)
-    remains = s[idx:]
-    lh_bp = remains.strip().split(" ")[:3]
-    print(lh_bp)
-
-    x = ' '.join(lh_bp)
-    idx2 = remains.find(x)+len(x)
-    print(remains[idx2:])
-    print(":" in remains)
-    print('###############')
+    return (lh_datetime, lh_sbp, lh_dbp, lh_hr), (rh_datetime, rh_sbp, rh_dbp, rh_hr)
 
 
-def grab_row(row):
-    with open('/tmp/foo.txt', 'a') as fo:
+def grab_row(row, rawtgtpath):
+    with open(rawtgtpath, 'a') as fo:
         fo.write(repr(row) + "\n")
 
 def find_bp_readings(tbl_as_extract):
@@ -116,10 +141,10 @@ def find_bp_readings(tbl_as_extract):
             yield row
     
 
-def table_to_df_captureonly(tbl_extract):
+def table_to_df_captureonly(tbl_extract, rawtgtpath):
 
     for row in find_bp_readings(tbl_extract):
-        grab_row(row)
+        grab_row(row, rawtgtpath)
     df = pd.DataFrame()
     return df
 
@@ -127,6 +152,9 @@ def table_to_df_captureonly(tbl_extract):
 def extract_pdf(filepath):
     """open `filepath` (assuming it is a formatted aktiia pdf report) and walk
     each page finding tables and extracting bp readings. """
+    # decide where to store the raw readings
+    basename = os.path.basename(filepath).replace(".pdf", ".raw")
+    rawpath = os.path.join(READINGS, basename)
     
     doc = pymupdf.open(filepath) # open a document
     bigtable = pd.DataFrame()
@@ -134,39 +162,53 @@ def extract_pdf(filepath):
         foundtables = page.find_tables(strategy='text')
         for tbl in foundtables.tables:
             extract = tbl.extract()
-            df = table_to_df_captureonly(extract)              
+            df = table_to_df_captureonly(extract, rawpath)              
                 
             print(f'page: {c} {page.number}')
-            if bigtable.empty:
-                bigtable = df
-            else:
-                bigtable = pd.concat([bigtable, df], axis=0)
-    return bigtable
+    return rawpath
 
+def parse_intermediate_row_data(rawpath):
+    ''' '''
+
+    bodyrows = []
+    with open(rawpath) as fo:
+        txt = fo.read()
+    for row in txt.split("\n"):
+        lh, rh = process_text_row(row)
+        if lh[0]:
+            bodyrows.append(lh)
+        if rh[0]:
+            bodyrows.append(rh)
+    df = pd.DataFrame(bodyrows, columns=['datetime', 'sbp', 'dbp','hr'])
+    return df
 
 def run(f):
-    # f :'/home/pbrian/Downloads/AktiiaReport_pb_Nov2024.pdf'
+    # walk through the pdf, extracting what we can into a intermedizte "raw"
+    # format.
     basename = os.path.basename(f)
-    dframe = extract_pdf(f)
-    newloc = os.path.join('/home/pbrian/Desktop/readings',
-                          basename.replace(".pdf", ".csv"))
-    #dframe.to_parquet(path=newloc)
-    dframe.to_csv(path_or_buf=newloc) #using csv because no network atm so cannot pip
+    rawpath = extract_pdf(f)
+    # now from the raw (intermediate) figures we have, parse into df
+    dframe = parse_intermediate_row_data(rawpath) 
+
+    newloc = os.path.join(READINGS, basename.replace(".pdf", ".parq"))
+ #   dframe.to_parquet(path=newloc)
     print(dframe)
 
-def foo():
-    folder = '/home/pbrian/Downloads/'
-    files = [f for f in os.listdir(folder) if f.startswith('Aktiia')]
+def run_all_pdfs_on_disk():
+    files = [f for f in os.listdir(FOLDER) if f.startswith('Aktiia')]
     for f in files:
-        fpath = os.path.join(folder, f)
+        fpath = os.path.join(FOLDER, f)
         print(fpath)
         run(fpath)
+        break
 
-
+FOLDER = '/home/pbrian/Downloads/'
+READINGS = '/home/pbrian/Desktop/readings' 
 if __name__ == '__main__':
-    test_process_rows()
-    #foo()
-
+    #x = convertdates("1June2400:19")
+    #print(x)
+    #test_process_rows()
+    run_all_pdfs_on_disk()
     #from docopt import docopt
     #args = docopt(HELPSTRING)
     #filepath = args['<filepath>']
